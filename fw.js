b16d1022a6e3080bc14d3b760073c150ac3eb4a5
Minko Gechev <mgechev@gmail.com>
2023-01-03 17:50:17 -0800

    Initial commit

---
+++ fw.js
@@ -0,0 +1,569 @@
+const data = {
+  children: [
+    {
+      name: "Fearful",
+      children: [
+        {
+          name: "Scared",
+          children: [{ name: "Helpless" }, { name: "Frightened" }],
+        },
+        {
+          name: "Anxious",
+          children: [
+            { name: "Overwhelmed" },
+            {
+              name: "Worried",
+            },
+          ],
+        },
+        {
+          name: "Insecure",
+          children: [
+            { name: "Inadequate" },
+            {
+              name: "Inferior",
+            },
+          ],
+        },
+        {
+          name: "Weak",
+          children: [
+            {
+              name: "Worthless",
+            },
+            {
+              name: "Insignificant",
+            },
+          ],
+        },
+        {
+          name: "Rejected",
+          children: [{ name: "Excluded" }, { name: "Persecuted" }],
+        },
+        {
+          name: "Threatened",
+          children: [{ name: "Nervous" }, { name: "Exposed" }],
+        },
+      ],
+    },
+    {
+      name: "Angry",
+      children: [
+        {
+          name: "Let down",
+          children: [
+            {
+              name: "Betrayed",
+            },
+            {
+              name: "Resentful",
+            },
+          ],
+        },
+        {
+          name: "Humiliated",
+          children: [
+            {
+              name: "Disrespected",
+            },
+            {
+              name: "Ridiculed",
+            },
+          ],
+        },
+        {
+          name: "Bitter",
+          children: [
+            {
+              name: "Indignant",
+            },
+            {
+              name: "Violated",
+            },
+          ],
+        },
+        {
+          name: "Mad",
+          children: [
+            {
+              name: "Furious",
+            },
+            {
+              name: "Jealous",
+            },
+          ],
+        },
+        {
+          name: "Aggressive",
+          children: [
+            {
+              name: "Provoked",
+            },
+            {
+              name: "Hostile",
+            },
+          ],
+        },
+        {
+          name: "Frustrated",
+          children: [
+            {
+              name: "Influriated",
+            },
+            {
+              name: "Annoyed",
+            },
+          ],
+        },
+        {
+          name: "Distant",
+          children: [
+            {
+              name: "Withdrawn",
+            },
+            {
+              name: "Numb",
+            },
+          ],
+        },
+        {
+          name: "Critical",
+          children: [
+            {
+              name: "Skeptical",
+            },
+            {
+              name: "Dismissive",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Disgusted",
+      children: [
+        {
+          name: "Disapproving",
+          children: [
+            {
+              name: "Judgmental",
+            },
+            {
+              name: "Embarrassed",
+            },
+          ],
+        },
+        {
+          name: "Disappointed",
+          children: [
+            {
+              name: "Appalled",
+            },
+            {
+              name: "Revolted",
+            },
+          ],
+        },
+        {
+          name: "Awful",
+          children: [
+            {
+              name: "Nauseated",
+            },
+            {
+              name: "Detestable",
+            },
+          ],
+        },
+        {
+          name: "Repelled",
+          children: [
+            {
+              name: "Horrified",
+            },
+            {
+              name: "Hesitant",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Sad",
+      children: [
+        {
+          name: "Hurt",
+          children: [
+            {
+              name: "Embarrassed",
+            },
+            {
+              name: "Disappointed",
+            },
+          ],
+        },
+        {
+          name: "Depressed",
+          children: [
+            {
+              name: "Inferior",
+            },
+            {
+              name: "Empty",
+            },
+          ],
+        },
+        {
+          name: "Guilty",
+          children: [
+            {
+              name: "Remorseful",
+            },
+            {
+              name: "Ashamed",
+            },
+          ],
+        },
+        {
+          name: "Despair",
+          children: [
+            {
+              name: "Grief",
+            },
+            {
+              name: "Powerless",
+            },
+          ],
+        },
+        {
+          name: "Vulnerable",
+          children: [
+            {
+              name: "Victimized",
+            },
+            {
+              name: "Fragile",
+            },
+          ],
+        },
+        {
+          name: "Lonely",
+          children: [
+            {
+              name: "Isolated",
+            },
+            {
+              name: "Abandoned",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Happy",
+      children: [
+        {
+          name: "Playful",
+          children: [
+            {
+              name: "Aroused",
+            },
+            {
+              name: "Cheeky",
+            },
+          ],
+        },
+        {
+          name: "Content",
+          children: [
+            {
+              name: "Free",
+            },
+            {
+              name: "Joyful",
+            },
+          ],
+        },
+        {
+          name: "Interested",
+          children: [
+            {
+              name: "Curious",
+            },
+            {
+              name: "Inquisitive",
+            },
+          ],
+        },
+        {
+          name: "Proud",
+          children: [
+            {
+              name: "Successful",
+            },
+            {
+              name: "Confident",
+            },
+          ],
+        },
+        {
+          name: "Accepted",
+          children: [
+            {
+              name: "Respected",
+            },
+            {
+              name: "Valued",
+            },
+          ],
+        },
+        {
+          name: "Powerful",
+          children: [
+            {
+              name: "Courageous",
+            },
+            {
+              name: "Creative",
+            },
+          ],
+        },
+        {
+          name: "Peaceful",
+          children: [
+            {
+              name: "Loving",
+            },
+            {
+              name: "Thankful",
+            },
+          ],
+        },
+        {
+          name: "Trusting",
+          children: [
+            {
+              name: "Sensitive",
+            },
+            {
+              name: "Intimate",
+            },
+          ],
+        },
+        {
+          name: "Optimistic",
+          children: [
+            {
+              name: "Hopeful",
+            },
+            {
+              name: "Inspired",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Surprised",
+      children: [
+        {
+          name: "Startled",
+          children: [
+            {
+              name: "Shocked",
+            },
+            {
+              name: "Dismayed",
+            },
+          ],
+        },
+        {
+          name: "Confused",
+          children: [
+            {
+              name: "Disillusioned",
+            },
+            {
+              name: "Perplexed",
+            },
+          ],
+        },
+        {
+          name: "Amazed",
+          children: [
+            {
+              name: "Astonished",
+            },
+            {
+              name: "Awe",
+            },
+          ],
+        },
+        {
+          name: "Excited",
+          children: [
+            {
+              name: "Eager",
+            },
+            {
+              name: "Energetic",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Bad",
+      children: [
+        {
+          name: "Bored",
+          children: [{ name: "Indifferent" }, { name: "Apathetic" }],
+        },
+        { name: "Busy", children: [{ name: "Pressured" }, { name: "Rushed" }] },
+        {
+          name: "Stressed",
+          children: [
+            { name: "Overwhelmed" },
+            {
+              name: "Out of control",
+            },
+          ],
+        },
+        {
+          name: "Tired",
+          children: [
+            { name: "Sleepy" },
+            {
+              name: "Unfocused",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+function treeify(source) {
+  const parents = [];
+  const nodes = source.trim().split(/\n/g);
+  parents.push({ children: [] });
+  for (let i = 0, n = nodes.length; i < n; ++i) {
+    const depth = nodes[i].match(/^\t*/)[0].length;
+    const parent = parents[depth];
+    if (!parent.children) parent.children = [];
+    parent.children.push(
+      (parents[depth + 1] = { name: nodes[i].slice(depth) })
+    );
+  }
+  return parents[0];
+}
+
+const partition = (data) =>
+  d3.partition().size([2 * Math.PI, radius])(d3.hierarchy(data).count());
+
+const color = d3
+  .scaleOrdinal()
+  // .range(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+  .range([
+    "#FFE08E",
+    "#FF8E8E",
+    "#8F8F8F",
+    "#8FBCE2",
+    "#FFFF8E",
+    "#BBA2D0",
+    "#8ED9AE",
+  ]);
+
+const element = d3.select("#wheel").node();
+const width = element.getBoundingClientRect().width; // get width of article so graphic is responsive
+const radius = width / 2;
+
+const arc = d3
+  .arc()
+  .startAngle((d) => d.x0)
+  .endAngle((d) => d.x1)
+  .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
+  .padRadius(radius / 2)
+  .innerRadius((d) => d.y0)
+  .outerRadius((d) => d.y1 - 1);
+
+chart = new (function () {
+  const root = partition(data);
+
+  let svg = d3
+    .select("#wheel")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", width)
+    .style("width", "100%")
+    .style("height", "auto")
+    .style("font", "0.8rem sans-serif");
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${width / 2})`);
+
+  g.append("g")
+    .attr("fill-opacity", 0.6)
+    .selectAll("path")
+    .data(root.descendants().slice(1))
+    .enter()
+    .append("path")
+    .attr("fill", (d) => {
+      while (d.depth > 1) d = d.parent;
+      return color(d.data.name);
+    })
+    .attr('data-name', d => {
+      return d.data.name;
+    })
+    .attr("d", arc);
+
+  g.append("g")
+    .attr("pointer-events", "none")
+    .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(root.descendants().slice(1))
+    .enter()
+    .append("text")
+    .attr("transform", function (d) {
+      const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
+      const y = (d.y0 + d.y1) / 2;
+      return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+    })
+    .attr("dy", "0.35em")
+    .text((d) => d.data.name);
+
+  return svg.node();
+})();
+
+(function initialize() {
+
+  const feelings = [];
+
+  function render() {
+    const c = document.querySelector('#feelings');
+    c.innerHTML = '';
+    for (const feeling of feelings) {
+      const item = document.createElement('li');
+      item.innerText = feeling;
+      c.appendChild(item);
+    }
+  }
+
+  function clear() {
+    feelings.length = 0;
+    render();
+  }
+
+  function clickHandler(e) {
+    feelings.push(e.target.getAttribute('data-name'))
+    render();
+  }
+
+  const paths = Array.from(document.querySelectorAll('path'));
+  paths.forEach(path => {
+    path.addEventListener('click', clickHandler);
+  });
+
+  document.querySelector('#clear-button').addEventListener('click', clear);
+}());
